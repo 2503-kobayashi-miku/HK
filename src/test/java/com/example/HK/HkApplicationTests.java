657fba81b9929f561d7d62e22aa56af57ec77d40
hirahara_hikaru <hirahara.hikaru@alhinc.jp>
2025-04-16 12:03:52 +0900

    first commit

---
+++ src/test/java/com/example/HK/HkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HK;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
