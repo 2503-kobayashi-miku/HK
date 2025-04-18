1f02cf75565961814a2433f919b815b44679a6d0
hirahara_hikaru <hirahara.hikaru@alhinc.jp>
2025-04-18 14:31:35 +0900

    コメント登録　実装

--- src/main/java/com/example/HK/controller/form/CommentForm.java
+++ src/main/java/com/example/HK/controller/form/CommentForm.java
@@ -2,6 +2,8 @@ package com.example.HK.controller.form;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotBlank;
 
 import java.time.LocalDateTime;
 
@@ -10,6 +12,8 @@ import java.time.LocalDateTime;
 public class CommentForm {
 
     private int id;
+    @NotBlank(message = "メッセージを入力してください")
+    @Length(max= 500, message = "500文字以内で入力してください")
     private String text;
     private int userId;
     private int messageId;
--- src/main/java/com/example/HK/service/CommentService.java
+++ src/main/java/com/example/HK/service/CommentService.java
@@ -51,6 +51,26 @@ public class CommentService {
     }
 
     /*
+     * レコード追加
+     */
+    public void saveComment(CommentForm reqComment) {
+        Comment saveComment = setCommentEntity(reqComment);
+        commentRepository.save(saveComment);
+    }
+
+    /*
+     * リクエストから取得した情報をEntityに設定
+     */
+    private Comment setCommentEntity(CommentForm reqComment) {
+        Comment comment = new Comment();
+        comment.setId(reqComment.getId());
+        comment.setText(reqComment.getText());
+        comment.setUserId(reqComment.getUserId());
+        comment.setMessageId(reqComment.getMessageId());
+        return comment;
+    }
+
+    /*
      * DBから取得したデータをFormに設定
      */
     private List<CommentForm> setCommentForm(List<Comment> results) {
