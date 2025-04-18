ef3aec217e08c0c6634e353e9aaae23a8a1170a2
hirahara_hikaru <hirahara.hikaru@alhinc.jp>
2025-04-18 10:50:57 +0900

    投稿登録　実装　※バリデーション実装

--- src/main/java/com/example/HK/controller/form/MessageForm.java
+++ src/main/java/com/example/HK/controller/form/MessageForm.java
@@ -2,6 +2,8 @@ package com.example.HK.controller.form;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotBlank;
 
 import java.time.LocalDateTime;
 
@@ -10,8 +12,14 @@ import java.time.LocalDateTime;
 public class MessageForm {
 
     private int id;
+    @NotBlank(message = "件名を入力してください")
+    @Length(max= 30, message = "件名は30文字以内で入力してください")
     private String title;
+    @NotBlank(message = "本文を入力してください")
+    @Length(max= 1000, message = "本文は1000文字以内で入力してください")
     private String text;
+    @NotBlank(message = "カテゴリを入力してください")
+    @Length(max= 10, message = "カテゴリは10文字以内で入力してください")
     private String category;
     private int userId;
     private LocalDateTime createdDate;
